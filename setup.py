9fa9ca7926f03abc5ab85a9832c42368b0a30c0a
davidluu21111 <114189553+davidluu21111@users.noreply.github.com>
2025-03-19 00:08:30 -0400

    folders added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Khoi Nguyen',
+    author_email = 'khoiluu2003@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
